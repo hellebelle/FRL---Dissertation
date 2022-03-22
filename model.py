01f1769dfa379a098dc1169a2b5da5a94200d799
hellebelle <maliakaa@tcd.ie>
2022-03-22 23:55:55 +0000

    added second reward function

--- model.py
+++ model.py
@@ -4,8 +4,10 @@ from msilib.schema import Class
 import os
 import sys
 import optparse
+from matplotlib import pyplot as plt
 from sumolib import checkBinary
 import numpy as np
+
 import traci
 
 class Q_Learning_Model:
@@ -37,7 +39,7 @@ class Q_Learning_Model:
         
     
     #define an epsilon greedy algorithm that will choose which action to take next (i.e., where to move next)
-    def get_next_action(self, current_row_index, current_column_index, epsilon):
+    def get_next_action(self, current_row_index, epsilon):
         #if a randomly chosen value between 0 and 1 is less than epsilon, 
         #then choose the most promising value from the Q-table for this state.
         if np.random.random() < epsilon:
@@ -59,69 +61,104 @@ class Q_Learning_Model:
 
         return row_index, column_index
 
+    def reset_simulation(self) :
+        traci.close()
+        traci.start([sumoBinary_gui, "-c", "N7_simulation.sumo.cfg", 
+                            "--tripinfo-output", "tripinfo.xml"])
+
     def run(self):
         #define training parameters
-        epsilon = 0.9 #the percentage of time when we should take the best action (instead of a random action)
+        epsilon = 0.9 #the percentage of time when we should take the best action (instead of a random action/exploit instead of explore)
         discount_factor = 0.9 #discount factor for future rewards
         learning_rate = 0.9 #the rate at which the agent should learn
-        
-        step = 0
-        
-        row_index, column_index = self.get_next_location()
 
+        waiting_times = []
+        rewards = []
+        episodes = 7
         # reward = 0
         
         # while step < 2000:
-        while traci.simulation.getMinExpectedNumber() > 0:
-            num_vehicles_at_det = traci.inductionloop.getLastStepVehicleNumber(self.det_id)
-            vehicles_at_det = traci.inductionloop.getLastStepVehicleIDs(self.det_id)
-            # num_vehicles = traci.vehicle.getIDCount()
-            # vehicles = traci.vehicle.getIDList()
-            # avg_waiting_time = 0
-            cummulative_waiting_time = 0
-            if num_vehicles_at_det > 0:
-                for i in vehicles_at_det:
-                    # avg_waiting_time += traci.vehicle.getWaitingTime(i)
-                    cummulative_waiting_time += traci.vehicle.getWaitingTime(i)
-            
-                # avg_waiting_time/=num_vehicles #avg_waiting_time of vehicle in N7
+        #
+        for i in range(episodes):
+             waiting_time_per_episode = []
+             rewards_per_episode = []
+             step = 0
+             print('Episode: ', i+1)
+             row_index, column_index = self.get_next_location()
+             while traci.simulation.getMinExpectedNumber() > 0:
+                num_vehicles_at_det = traci.inductionloop.getLastStepVehicleNumber(self.det_id)
+                vehicles_at_det = traci.inductionloop.getLastStepVehicleIDs(self.det_id)
+                # num_vehicles = traci.vehicle.getIDCount()
+                # vehicles = traci.vehicle.getIDList()
+                # avg_waiting_time = 0
                 
-                occupancy = traci.inductionloop.getLastStepOccupancy(self.det_id)
-                #choose which action to take (i.e., where to move next)
-                action_index = self.get_next_action(row_index, column_index, epsilon)
-                # print('action:', action_index)
-
-                # if occupancy == 0: avg_waiting_time = 0 #if lane is unoccupied, waiting time on lane will be 0 (e.g while occupancy = 0,  avg_waiting_time * occupancy = 0)
-
-                #perform the chosen action, and transition to the next state (i.e., move to the next location)
-                old_row_index, old_column_index = row_index, column_index #store the old row and column indexes
-                traci.trafficlight.setPhase(TL_id, action_index)
-                row_index, column_index = self.get_next_location()
-
-                #receive the reward for moving to the new state, and calculate the temporal difference
-                # reward = self.rewards[row_index, column_index]
-
-                # reward = occupancy - avg_waiting_time
-                k = 2
-                if cummulative_waiting_time > 0 and occupancy > 0 :
-                   reward = ((k**2)*occupancy)/cummulative_waiting_time
-                elif (cummulative_waiting_time > 0 and occupancy == 0):
-                   reward = -k*cummulative_waiting_time
-                else:
-                   reward = (k**occupancy)
-                print('Reward', reward)
-                old_q_value = self.q_values[old_row_index, old_column_index]
-                # print("Old Q-val: ", old_q_value)
-                temporal_difference = reward + (discount_factor * np.max(self.q_values[row_index, column_index])) - old_q_value
-                #update the Q-value for the previous state and action pair
-                new_q_value = old_q_value + (learning_rate * temporal_difference)
-                self.q_values[old_row_index, old_column_index] = new_q_value
+                cummulative_waiting_time = 0
+                if num_vehicles_at_det > 0:
+                    for i in vehicles_at_det:
+                        # avg_waiting_time += traci.vehicle.getWaitingTime(i)
+                        cummulative_waiting_time += traci.vehicle.getWaitingTime(i)
                 
-            traci.simulationStep()
-            step +=1
+                    # avg_waiting_time/=num_vehicles #avg_waiting_time of vehicle in N7
+                    # time_step.append(step)
+                    waiting_time_per_episode.append(cummulative_waiting_time)
+                    occupancy = traci.inductionloop.getLastStepOccupancy(self.det_id)
+                    #choose which action to take (i.e., where to move next)
+                    action_index = self.get_next_action(row_index, epsilon)
+                    # print('action:', action_index)
 
-        # print(traci.lane.getIDList())
-        # print(self.q_values)
+                    # if occupancy == 0: avg_waiting_time = 0 #if lane is unoccupied, waiting time on lane will be 0 (e.g while occupancy = 0,  avg_waiting_time * occupancy = 0)
+
+                    #perform the chosen action, and transition to the next state (i.e., move to the next location)
+                    old_row_index, old_column_index = row_index, column_index #store the old row and column indexes
+                    traci.trafficlight.setPhase(TL_id, action_index)
+                    row_index, column_index = self.get_next_location()
+
+                    #receive the reward for moving to the new state, and calculate the temporal difference
+                    # reward = self.rewards[row_index, column_index]
+
+                    # reward = occupancy - avg_waiting_time
+                    # k = 2
+                    # if cummulative_waiting_time > 0 and occupancy > 0 :
+                    #     reward = ((k**2)*occupancy)/cummulative_waiting_time
+                    # elif (cummulative_waiting_time > 0 and occupancy == 0):
+                    #     reward = -k*cummulative_waiting_time
+                    # else:
+                    #     reward = (k*occupancy)
+                    
+                    if (occupancy >= 0 and occupancy <= 49):
+                        reward = -10
+                    elif (occupancy > 50 and occupancy <= 79):
+                        reward = 10
+                    else:
+                        occupancy = 100
+
+                    rewards_per_episode.append(reward)
+                    print('Reward', reward)
+                    old_q_value = self.q_values[old_row_index, old_column_index]
+                    # print("Old Q-val: ", old_q_value)
+                    temporal_difference = reward + (discount_factor * np.max(self.q_values[row_index, column_index])) - old_q_value
+                    #update the Q-value for the previous state and action pair
+                    new_q_value = old_q_value + (learning_rate * temporal_difference)
+                    self.q_values[old_row_index, old_column_index] = new_q_value
+                    
+                traci.simulationStep()
+                step +=1
+             waiting_times.append(np.average(waiting_time_per_episode))
+             rewards.append(np.sum(rewards_per_episode))
+             self.reset_simulation()
+        fig, ax = plt.subplots(1) # Creates figure fig and add an axes, ax.
+        fig2, ax2 = plt.subplots(1) # Another figure
+        ax.plot(range(1,episodes+1), waiting_times) #Add a straight line to the axes of the first figure.
+        ax.set_title('Cummulative Waiting Time of Vehicles per Episode')
+        ax.set_xlabel('Episode')
+        ax.set_ylabel('Cummulative Waiting Time of Vehicles')
+        ax2.plot(range(1,episodes+1), rewards) #Add a straight line to the axes of the first figure.
+        ax2.set_title('Rewards per Episode')
+        ax2.set_xlabel('Episode')
+        ax2.set_ylabel('Rewards')
+        fig.show() #Only shows figure 1 and removes it from the "current" stack.
+        fig2.show() #Only shows figure 2 and removes it from the "current" stack.
+        plt.show() #Does not show anything, because there is nothing in the "current" stack.
         return self.q_values
 
 
@@ -132,36 +169,6 @@ if 'SUMO_HOME' in os.environ:
 else: 
     sys.exit("please declare environment variable 'SUMO_HOME")
 
-# #define the shape of the environment (i.e., its states and actions)
-# environment_rows = 3
-# environment_columns = 3
-
-# #Create a 2D numpy array to hold the current Q-values for each state and action pair: Q(s, a) 
-# #The array contains 3 rows and 3 columns (to match the shape of the environment), as well as a third "action" dimension.
-# #The "action" dimension consists of 4 layers that will allow us to keep track of the Q-values for each possible action in
-# #each state (see next cell for a description of possible actions). 
-# #The value of each (state, action) pair is initialized to 0.
-# q_values = np.zeros((environment_rows, environment_columns))
-
-# #define actions
-# #numeric action codes: 0 = green, 1 = yellow, 2 = red
-# actions = [0, 1, 2]
-
-# #define states
-# #numeric state codes: 0 = 0-49% occupancy, 1 = 50-79% occupancy, 2 = 80-100% occupancy
-# states = [0, 1, 2]
-
-#Create a 2D numpy array to hold the rewards for each state. 
-#The array contains 3 rows and 3 columns (to match the shape of the environment), and each value is initialized to -100.
-# rewards = np.full((environment_rows, environment_columns), -100)
-# rewards[0,0] = 100;  rewards[2,2] = 100
-# rewards[0,2] = -100; rewards[2,0] = -100
-# rewards[0,1] = 0
-# rewards[1,0] = 10; rewards[1,1] = 10; rewards[1,2] = 10
-# rewards[2,1] = 50
-
-# for row in rewards:
-#     print(row)
 
 # traffic_light_ID_list = traci.trafficlight.getIDList()
 # det_list = traci.inductionloop.getIDList()
@@ -190,10 +197,11 @@ def get_options():
     
 if __name__ == "__main__":
    
-    sumoBinary_gui = os.path.join(os.environ['SUMO_HOME'], 'bin', 'sumo-gui.exe')
+    sumoBinary_gui = os.path.join(os.environ['SUMO_HOME'], 'bin', 'sumo.exe')
     
     traci.start([sumoBinary_gui, "-c", "N7_simulation.sumo.cfg", 
                             "--tripinfo-output", "tripinfo.xml"])
+    
 
     # models = []
     # for key, value in TL_det_lookup.items():
@@ -217,6 +225,10 @@ if __name__ == "__main__":
 #  [ 1.21460614e+02  1.28456999e+01  1.53019153e+01]
 #  [-1.34759940e+01  2.12943863e+02  2.16636786e+01]]
 
+
+
+########### INCORRECT LOGIC BUT GIVES IDEAL Q-TABLE ###############
+
 # k = 5
 # if avg_waiting_time > 0 and occupancy > 0 :
 #    reward = ((k**2)*occupancy)/avg_waiting_time
@@ -229,6 +241,8 @@ if __name__ == "__main__":
 #  [6.43747523e+04 9.50520194e+02 1.00507304e+04]
 #  [1.39726221e+04 1.24829310e+03 3.29424270e+04]]
 
+
+########### THIS SHOULD BE WHAT WORKS ###############
 # k = 2
 # if avg_waiting_time > 0 and occupancy > 0 :
 #     reward = ((k**2)*occupancy)/avg_waiting_time
@@ -241,6 +255,8 @@ if __name__ == "__main__":
 #  [ 2.46033331e+02  1.13448398e+02  1.16889957e+04]
 #  [ 1.39984045e+03  3.46826019e+02  4.80364964e+05]]
 
+
+
 # k = 2
 # if avg_waiting_time > 0 and occupancy > 0 :
 #     reward = ((k**2)*occupancy)/avg_waiting_time
@@ -252,6 +268,7 @@ if __name__ == "__main__":
 # [[1.35074488e+00 1.38317658e+00 1.34113797e+00]
 #  [4.63424480e+01 8.49249446e+03 2.06800057e+03]
 #  [1.50350562e+03 2.43884258e+04 6.25764064e+01]]
+
 
 # if avg_waiting_time > 1 :
 #     reward = occupancy/avg_waiting_time
@@ -273,9 +290,16 @@ if __name__ == "__main__":
 # [[1.84044701e-01 1.90123903e-01 2.03659049e-01]
 #  [6.71046897e+01 8.47560800e+03 4.87558679e+03]
 #  [6.01085943e+05 1.50306211e+04 1.64330011e+06]]
+
+# [[446.83570321 591.39185689 582.49075259]
+#  [518.40386347 416.71305832 690.27114661]
+#  [732.97608372 546.02835463 507.69869849]]
     TL_1 = Q_Learning_Model('2801474636', TL_det_lookup[TL_id])
     TL_1_Q_table = TL_1.run()
-    print(TL_1.q_values)
+    # print(TL_1.q_values)
+
+    
+    # fig.show() # Shows figure 1 again. You can show it as many times as you want.
 
     # run()
     # for i in models:
